e5260b905cd66535ee92b82f249e68a4f33c60e6
Vikash Yadav <32521687+vikash212000yadav@users.noreply.github.com>
2020-06-21 22:53:02 +0530

    models.py updated

--- HR/models.py
+++ HR/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 from django.utils import timezone
 from accounts.models import Patient
+
+
+# Payment Models
 class Payment(models.Model):
     pmid=models.AutoField(primary_key=True)
     patient = models.ForeignKey(Patient,on_delete=models.CASCADE)
@@ -8,5 +11,6 @@ class Payment(models.Model):
     paid=models.IntegerField(default=0)
     total=models.IntegerField(default=0)
     date=models.DateTimeField(default=timezone.now)
+    
     def __str__(self):
         return str(self.pmid)
